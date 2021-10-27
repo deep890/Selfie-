7bf2a9467f232afc3d1a86cc719b11b14a3c90ee
deep890 <64359511+deep890@users.noreply.github.com>
2021-10-27 20:40:39 +0530

    Add files via upload

---
+++ blur.py
@@ -0,0 +1,12 @@
+# import cv2
+# import matplotlib.pyplot as plt
+# im = cv2.imread('house.jpeg')
+# dst = cv2.GaussianBlur(im,(5,5),cv2.BORDER_DEFAULT)
+# plt.imshow(dst)
+# plt.show()
+# # import matplotlib as mpl 
+# # print(mpl. __version__)
+
+from platform import python_version
+
+print(python_version())
